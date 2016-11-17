c6473c4a88c377af9c07dcf8f7324e0e877719e9
Vladimir Nevinniy <vladimirnevinniy@MyOS.local>
2016-11-17 17:50:38 +0200

    First

--- GCDSamples/ViewController.swift
+++ GCDSamples/ViewController.swift
@@ -10,9 +10,13 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+       
+        
     }
 
     override func didReceiveMemoryWarning() {
@@ -21,5 +25,128 @@ class ViewController: UIViewController {
     }
 
 
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+         //simpleQueues()
+        
+        //queuesWithQoS()
+        
+        
+         concurrentQueues()
+         if let queue = inactiveQueue {
+            queue.activate()
+         }
+        
+        
+        // queueWithDelay()
+        
+        // fetchImage()
+        
+        // useWorkItem()
+    }
+    
+    
+    
+    func simpleQueues() {
+        
+        let queue = DispatchQueue(label: "com.nvovap.myqueue")
+        
+        queue.async {
+            for i in 0..<10 {
+                print("🔴 ", i)
+            }
+        }
+        
+        
+        let queueT2 = DispatchQueue(label: "com.nvovap.myqueue2")
+        
+        queueT2.async {
+            for i in 0..<10 {
+                print("🔵 ", i)
+            }
+        }
+        
+        
+        queue.sync {
+            for i in 0..<10 {
+                print("🔶 ", i)
+            }
+        }
+        
+        for i in 100..<110 {
+            print("Ⓜ️ ", i)
+        }
+        
+        
+        
+        
+    }
+    
+    
+    func queuesWithQoS() {
+        let queue1 = DispatchQueue(label: "com.nvovap.queue1", qos: DispatchQoS.userInitiated)
+        let queue2 = DispatchQueue(label: "com.nvovap.queue2", qos: DispatchQoS.utility)
+        
+        queue1.async {
+            for i in 0..<10 {
+                print("🔴 ", i)
+            }
+        }
+        
+        
+        queue2.async {
+            for i in 100..<110 {
+                print("🔵 ", i)
+            }
+        }
+        
+        for i in 1000..<1010 {
+            print("Ⓜ️", i)
+        }
+        
+    }
+    
+    
+    var inactiveQueue: DispatchQueue!
+    func concurrentQueues() {
+        let anotherQueue = DispatchQueue(label: "com.nvovap.anotherQueue", qos: .utility, attributes: [.initiallyInactive, .concurrent])
+        
+        inactiveQueue = anotherQueue
+        
+        anotherQueue.async {
+            for i in 0..<10 {
+                 print("🔴 ", i)
+            }
+        }
+        
+        anotherQueue.async {
+            for i in 100..<110 {
+                print("🔵 ", i)
+            }
+        }
+        
+        anotherQueue.async {
+            for i in 1000..<1010 {
+                print("⚫️ ", i)
+            }
+        }
+        
+    }
+    
+    
+    func queueWithDelay() {
+        
+    }
+    
+    
+    func fetchImage() {
+        
+    }
+    
+    
+    func useWorkItem() {
+        
+    }
 }
 
